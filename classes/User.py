acc42c3c9d8c3318ecf9b4757154fd586b48bfd9
Van <51791613+pavaniravella@users.noreply.github.com>
2021-12-14 21:09:56 -0500

    updated user class to change user password

--- classes/User.py
+++ classes/User.py
@@ -5,4 +5,7 @@ class User:
         self.password = password
     def hashPassowrd(password):
         return hashlib.sha256(str.encode(password)).hexdigest()
+    def changePasword(password):
+        self.passowrd = hashlib.sha256(str.encode(password)).hexdigest()
+        #figure out how to invoke and call this in the front end
     #need to figure out how to add it
