0608e8362241210be50a4a8bb91d8ab8fd057083
rhit-fayoumoa <fayoumoa@rose-hulman.edu>
2022-02-17 17:28:01 -0500

    final fix for supabase and transfer to services/supabase subdir

--- supabase/studio/hooks/queries/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './useOrganizationDetail'
-export * from './useProfile'
-export * from './useProjectAuthConfig'
--- supabase/studio/lib/common/fetch/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './base'
-export * from './delete'
-export * from './get'
-export * from './patch'
-export * from './post'
